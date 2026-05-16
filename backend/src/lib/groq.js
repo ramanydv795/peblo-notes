16972f6d17dd1e0a63631e15c3f555cf36122b02
ramanydv795 <raman@gmail.com>
2026-05-17 00:20:10 +0530

    update groq api version and version and fixed integrarion

--- backend/src/lib/groq.js
+++ backend/src/lib/groq.js
@@ -5,7 +5,7 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const generateNoteInsights = async (content, title) => {
   try {
     const response = await groq.chat.completions.create({
-      model: "llama3-8b-8192",
+      model: "llama-3.3-70b-versatile",
       messages: [
         {
           role: "system",
